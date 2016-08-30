8050830ea863ea6b666b65c99c00715eb7ff7b11
Nicole Cordes <cordes@cps-it.de>
2016-08-31 00:01:07 +0200

    [TASK] Namespace AbstractUserAuthentication hook

---
+++ Classes/Hook/SwitchUserRoleHook.php
@@ -0,0 +1,64 @@
+<?php
+namespace CPSIT\BegroupsRoles\Hook;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2016 Nicole Cordes <cordes@cps-it.de>, CPS-IT GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Core\Authentication\AbstractUserAuthentication;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Sets current user group
+ */
+class SwitchUserRoleHook
+{
+    /**
+     * Assign user group from session data
+     */
+    public function setUserGroup()
+    {
+        $backendUser = $this->getBackendUser();
+        if (!empty($backendUser->user['tx_begroupsroles_enabled'])
+            && strpos($backendUser->user[$backendUser->usergroup_column], ',') !== false
+        ) {
+            $role = (int)$backendUser->getSessionData('tx_begroupsroles_role');
+            if (!empty($role)
+                && GeneralUtility::inList($backendUser->user[$backendUser->usergroup_column], $role)
+            ) {
+                $backendUser->user[$backendUser->usergroup_column] = $role;
+            } else {
+                $backendUser->setAndSaveSessionData('tx_begroupsroles_role', 0);
+            }
+        }
+    }
+
+    /**
+     * @return BackendUserAuthentication
+     */
+    protected function getBackendUser()
+    {
+        return $GLOBALS['BE_USER'];
+    }
+}
--- ext_tables.php
+++ ext_tables.php
@@ -3,7 +3,7 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-if (TYPO3_MODE === 'BE') {
+if ('BE' === TYPO3_MODE) {
     \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Core\Imaging\IconRegistry::class)->registerIcon(
         'begroups-roles-switchUserGroup',
         \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
@@ -11,10 +11,4 @@ if (TYPO3_MODE === 'BE') {
             'source' => 'EXT:begroups_roles/Resources/Public/Icons/SwitchUserGroup.png',
         ]
     );
-
-	// Register hook to adjust current user group
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp']['begroups_roles'] =
-		'EXT:begroups_roles/Classes/Hook/Userauth.php:Tx_BegroupsRoles_Hook_Userauth->setUserGroup';
 }
-
-?>
