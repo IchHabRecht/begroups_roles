6f269ad978ab7f42c17b31c30c906f5fcfb9db71
Nicole Cordes <cordes@cps-it.de>
2016-08-30 16:58:32 +0200

    [TASK] Move TCA to Configuration folder

--- ext_tables.php
+++ ext_tables.php
@@ -10,23 +10,6 @@ if (TYPO3_MODE === 'BE') {
 	);
 	t3lib_SpriteManager::addSingleIcons($icons, 'begroups-roles');
 
-	// Add checkbox to be_users table
-	$tempColumns = array(
-		'tx_begroupsroles_enabled' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:begroups_roles/Resources/Private/Language/locallang_db.xml:be_users.tx_begroupsroles_enabled',
-			'config' => array(
-				'type' => 'check',
-				'defaukt' => 0,
-			),
-		),
-	);
-	if (version_compare(TYPO3_branch, '6.1', '<')) {
-		t3lib_div::loadTCA('be_users');
-	}
-	t3lib_extMgm::addTCAcolumns('be_users', $tempColumns);
-	t3lib_extMgm::addToAllTCAtypes('be_users', 'tx_begroupsroles_enabled;;;;1-1-1', '', 'after:usergroup');
-
 	// Register hook to adjust current user group
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp']['begroups_roles'] =
 		'EXT:begroups_roles/Classes/Hook/Userauth.php:Tx_BegroupsRoles_Hook_Userauth->setUserGroup';
