fc48a457d9f315c2f1649b2052ff7e7e157a13b9
Nicole Cordes <cordes@cps-it.de>
2016-08-30 16:39:33 +0200

    [TASK] Raise TYPO3 version to 7.6.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.0.0-dev',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-6.2.99',
+			'typo3' => '7.6.0-8.3.99',
 		),
 		'conflicts' => array(
 		),
