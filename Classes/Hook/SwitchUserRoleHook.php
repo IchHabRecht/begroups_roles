66da84401fe43e3882611b43ab357f421899685a
Nicole Cordes <cordes@cps-it.de>
2017-04-27 13:22:05 +0200

    [TASK] Fix coding style according to php-cs-fixer

--- Classes/Hook/SwitchUserRoleHook.php
+++ Classes/Hook/SwitchUserRoleHook.php
@@ -25,7 +25,6 @@ namespace CPSIT\BegroupsRoles\Hook;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-use TYPO3\CMS\Core\Authentication\AbstractUserAuthentication;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
