7eef6398ba50b0506112f17a5c3da0a459aba885
Nicole Cordes <typo3@cordes.co>
2017-10-29 16:01:54 +0100

    [FEATURE] Add new configuration option "limit" to be_users TCA

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,5 +2,6 @@
 # Table structure for table 'be_users'
 #
 CREATE TABLE be_users (
-	tx_begroupsroles_enabled tinyint(4) unsigned DEFAULT '0' NOT NULL
+	tx_begroupsroles_enabled tinyint(4) UNSIGNED DEFAULT '0' NOT NULL,
+	tx_begroupsroles_limit tinyint(4) UNSIGNED DEFAULT '0' NOT NULL
 );
