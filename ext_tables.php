6f1f4e4bbd4b4b02c2e57d7296ca24c77b8a2c59
Nicole Cordes <typo3@cordes.co>
2017-11-30 13:31:02 +0100

    [FEATURE] Add context-sensitive help labels

--- ext_tables.php
+++ ext_tables.php
@@ -4,6 +4,8 @@ if (!defined('TYPO3_MODE')) {
 }
 
 if ('BE' === TYPO3_MODE) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('be_users', 'EXT:begroups_roles/Resources/Private/Language/locallang_csh.xlf');
+
     \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Core\Imaging\IconRegistry::class)->registerIcon(
         'begroups-roles-switchUserGroup',
         \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
